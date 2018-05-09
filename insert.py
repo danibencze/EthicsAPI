411662666c66ab9c17fb6a39f6cb8a7e7254f5c2
danibencze <BenczeDA@cardiff.ac.uk>
2018-05-09 23:14:34 +0100

    adding a file

---
+++ insert.py
@@ -0,0 +1,17 @@
+import pymysql.cursors
+
+connection = pymysql.connect(host='csmysql.cs.cf.ac.uk',
+                             user='group12.2017',
+                             password='rwqeBE4DBj5vdk',
+                             db='group12_2017',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+	with connection.cursor() as cursor:
+		sql = "INSERT INTO General (Title) VALUES ('Asd')"
+		cursor.execute(sql)
+		result = cursor.fetchall()
+		print(result)
+finally:
+	connection.close()
